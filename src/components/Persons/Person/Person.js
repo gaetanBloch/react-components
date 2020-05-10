06f87ac022c515c745ff8115795e754fe982e8ea
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-11 00:21:39 +0200

    Render Adjacent JSX elements by wrapping them in an array

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import styles from './Person.module.css'
-
 class Person extends Component {
 
   render() {
@@ -11,14 +9,18 @@ class Person extends Component {
       cursor: 'pointer'
     };
 
-    return (
-      <div className={styles.Person}>
-        <p onClick={this.props.click} style={pStyle}>
-          I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <input type="text" onChange={this.props.nameChanged} defaultValue={this.props.name} />
-      </div>
-    )
+    return [
+      // <div className={styles.Person}>
+      <p key="1" onClick={this.props.click} style={pStyle}>
+        I'm {this.props.name} and I am {this.props.age} years old!
+      </p>,
+      <input
+        key="2"
+        type="text"
+        onChange={this.props.nameChanged}
+        defaultValue={this.props.name} />
+      // </div>
+    ]
   }
 }
 
