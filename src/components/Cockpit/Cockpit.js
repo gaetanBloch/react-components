03940cf3760f291639324797c7453c0688eb68a6
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-10 23:24:13 +0200

    Use the useEffect React Hook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import styles from './Cockpit.module.css'
 
 const Cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+
+    // Faking Http request
+    setTimeout(() => {
+      alert('Saved data to cloud!')
+    }, 1000)
+
+    // Execute it when props.personLength is updated
+  // }, [props.personsLength]);
+
+    // Execute it only once
+  }, []);
+
   const classes = [];
   if (props.personsLength <= 2) {
     classes.push(styles.red);
