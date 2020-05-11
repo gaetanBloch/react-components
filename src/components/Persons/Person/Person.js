cfc5584cd9caa2a5c1f52bb1efbcaaccd58a99cf
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-11 02:14:42 +0200

    Use Context API static contextType in Class Component to replace a chain of props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,9 +12,12 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log('Authenticated: ' + this.context.authenticated)
   }
 
   render() {
@@ -29,9 +32,7 @@ class Person extends Component {
       // <React.Fragment>
       // <WithClass classes={styles.Person}>
       <Auxiliary>
-        <AuthContext.Consumer>
-          {context => context.authenticated ? <p>Authenticated!</p> : <p>Please login</p>}
-        </AuthContext.Consumer>
+        {this.context.authenticated ? <p>Authenticated!</p> : <p>Please login</p>}
         <p onClick={this.props.click} style={pStyle}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
